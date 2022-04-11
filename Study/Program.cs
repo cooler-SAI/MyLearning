df633cc3e28d44e49ac415054c721e58159a38cf
cooler-SAI <cooler19860212@yahoo.com>
2022-04-11 21:38:52 +0300

    Code: added DecimalSeparator

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Study
 {
@@ -6,6 +7,11 @@ namespace Study
     {
         static void Main(string[] args)
         {
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
             double d = 10.2456524;
             Console.WriteLine(d.ToString("#.###"));
 
