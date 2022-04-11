bbf71f5da24b17d74e183121113a20af28087cee
cooler-SAI <cooler19860212@yahoo.com>
2022-04-11 23:15:50 +0300

    Code: formation style

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,7 +6,8 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            
+            double a = 1.256;
+            int b = (int)a;
 
             Console.ReadLine();
         }
