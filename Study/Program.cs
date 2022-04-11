8783d055c440d4c9df5c2d518ef7016083f19140
cooler-SAI <cooler19860212@yahoo.com>
2022-04-11 21:16:56 +0300

    Code: test up string

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,8 +6,18 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            var number = 50;
-            Console.WriteLine(number);
+            string firstName = "Ander";
+            string surName = "Shatohin";
+            string age = "38";
+            string specialization = "Intern";
+
+            string pattern = "Name: {0}, Surname: {1}, Age: {2}, Specialization: {3}";
+            Console.WriteLine(pattern,
+                              firstName,
+                              surName,
+                              age,
+                              specialization);
+
             Console.ReadLine();
         }
     }
