ee1d6b870666900b9bc7491e7cf49b5ed0102efa
cooler-SAI <cooler19860212@yahoo.com>
2022-04-11 18:57:23 +0300

    Code: created Study 2 for homework. Optimize code!

---
+++ Study2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Study2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write(" !!! ");
+        }
+    }
+}
