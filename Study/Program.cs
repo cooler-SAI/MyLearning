5b9bfe2a363a4445617630c4253e98b024a5b57a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-11 18:50:49 +0300

    Code: added ReadKey

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,7 +7,7 @@ namespace Study
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
