cc1bf838ba28df0e55103e3b011dd82e1cd998e1
cooler-SAI <cooler19860212@yahoo.com>
2022-04-12 21:02:18 +0300

    Code: added tempreture to commit

--- Study/Program.cs
+++ Study/Program.cs
@@ -11,11 +11,11 @@ namespace Study
             int OutsideTemp = rand.Next(-10, 20);
             if (OutsideTemp <= 0)
             {
-                Console.WriteLine("Cold outside! get a warm clothes");
+                Console.WriteLine($"Cold outside! {OutsideTemp}°с get a warm clothes");
             }
             else
             {
-                Console.WriteLine("It's hot! Get Lightweight clothing");
+                Console.WriteLine($"It's hot! {OutsideTemp}°с Get Lightweight clothing");
             }
 
             Console.ReadLine();
