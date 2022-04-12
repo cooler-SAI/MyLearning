0f41b1a1ffeebb0bd7495390a8c8edf33025d7e4
cooler-SAI <cooler19860212@yahoo.com>
2022-04-12 21:06:48 +0300

    clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,15 +8,7 @@ namespace Study
         {
             Random rand = new Random();
 
-            int OutsideTemp = rand.Next(-10, 20);
-            if (OutsideTemp <= 0)
-            {
-                Console.WriteLine($"Cold outside! {OutsideTemp}°с get a warm clothes");
-            }
-            else
-            {
-                Console.WriteLine($"It's hot! {OutsideTemp}°с Get Lightweight clothing");
-            }
+            
 
             Console.ReadLine();
         }
