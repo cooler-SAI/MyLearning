40c2d3a25f4b1564557fce8e612738f886102f64
cooler-SAI <cooler19860212@yahoo.com>
2022-04-13 23:26:17 +0300

    Code: learning while

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,7 +6,13 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Numbers from 10 to 0 :");
+            int number = 0;
+            while (number < 10)
+            {
+                Console.WriteLine($"{10 - number}");
+                number++;
+            }
 
             
         }
