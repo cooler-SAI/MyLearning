fcea0c8cecfda2e6b29c8bbd0f7972932bcbbc25
cooler-SAI <cooler19860212@yahoo.com>
2022-04-13 22:07:47 +0300

    Code: some tests here

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,7 +8,7 @@ namespace Study
         {
             
 
-            for (int i = 10; i <= 10 && i >= 0; --i)
+            for (int i = 1; i <= 10; i += 3)
             {
                 Console.WriteLine(i);
                 
