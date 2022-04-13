f3cf7c3b711e5c12e5edcaf13c6d35b58ce2faac
cooler-SAI <cooler19860212@yahoo.com>
2022-04-13 20:27:48 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,30 +6,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-
-            int firstNumber = rand.Next(-10, 11);
-            int secondNumber = rand.Next(-10, 11);
-
-            int maxValue, minValue;
-
-            if (firstNumber > secondNumber)
-            {
-                maxValue = firstNumber;
-                minValue = firstNumber;
-                Console.WriteLine($"MaxValue is : {firstNumber}");
-                Console.WriteLine($"MinValue is : {secondNumber}");
-            }
-            else if (firstNumber < secondNumber)
-            {                
-                Console.WriteLine($"MaxValue is : {secondNumber}");
-                Console.WriteLine($"MinValue is : {firstNumber}");
-            }
-            else
-            {
-                Console.WriteLine($"MaxValue is : {secondNumber}");
-                Console.WriteLine($"MinValue is : {firstNumber}");
-            }
+            
 
             Console.ReadLine();
         }
