4370afa1be861aa02a407cd9a824d51daa8b1b9b
cooler-SAI <cooler19860212@yahoo.com>
2022-04-14 21:31:23 +0300

    Code: test program

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,9 +6,18 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the number:");
+            int number = int.Parse(Console.ReadLine());
 
+            Random rand = new Random();
+            int[] numbersConsole = new int[number];
 
+            for (int i = 0; i < numbersConsole.Length; i++)
+            {
+                numbersConsole[i] = rand.Next(-5, 6);
+                Console.Write($"{numbersConsole[i]} ");
+            }
+            Console.ReadKey();
 
         }
     }
