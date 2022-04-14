5111d24288badbf12bec587f2be12b3001b01bbb
cooler-SAI <cooler19860212@yahoo.com>
2022-04-14 20:42:06 +0300

    Code: update arrays

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,13 +6,15 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            int[] varryable;
-            varryable = new int[3];
+            
 
             int[] array = new int[3];
+
             array[0] = 1;
             array[1] = 20;
             array[2] = 42;
+
+            Console.WriteLine(array[1]);
 
         }
     }
