3e7b08e1bf5a341fb2e724bd51c9261c698bc3c3
cooler-SAI <cooler19860212@yahoo.com>
2022-04-14 19:57:21 +0300

    Code: new system

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,7 +6,10 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            
+            int[] varryable;
+            varryable = new int[3];
+
+            int[] varryable2 = new int[3];
 
         }
     }
