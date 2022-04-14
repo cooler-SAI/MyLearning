65bc277b33895be68d71b255ceb039a92623559d
cooler-SAI <cooler19860212@yahoo.com>
2022-04-14 20:00:20 +0300

    Code: study arrays

--- Study/Program.cs
+++ Study/Program.cs
@@ -10,6 +10,9 @@ namespace Study
             varryable = new int[3];
 
             int[] array = new int[3];
+            array[0] = 1;
+            array[1] = 20;
+            array[2] = 42;
 
         }
     }
