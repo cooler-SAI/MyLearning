cf7d15c9b723954c1f117b30819996e1c9359bb3
cooler-SAI <cooler19860212@yahoo.com>
2022-04-15 20:57:22 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,16 +7,7 @@ namespace Study
         static void Main(string[] args)
         {
 
-            int[] arrayOne = new int[5] { 42, 16, 22, 60, 5 };
-
-            int[] arrayTwo = new int[5] { 12, 3, 8, 42, 6 };
-
-            Array.Clear(arrayOne, 2, 1);
-            Array.Reverse(arrayTwo);
             
-
-            Console.WriteLine($"your Array One Data: {arrayOne[0]} {arrayOne[1]} {arrayOne[2]} {arrayOne[3]} {arrayOne[4]}");
-            Console.WriteLine($"your Array Two Data: {arrayTwo[0]} {arrayTwo[1]} {arrayTwo[2]} {arrayTwo[3]} {arrayTwo[4]}");
         }
     }
 }
