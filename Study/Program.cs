a039c5483062bca4a2dd3e56ba4cf82ba56e0a6a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-15 23:55:11 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,23 +6,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-
-            int[,] table = new int[4, 4];
-
-            for (int i = 1; i < table.GetLength(0); i++)
-            {
-                int mult = 1;
-
-                for (int j = 1; j < table.GetLength(1); j++)
-                {
-                    table[i, j] = rand.Next(1, 10);
-                    mult *= table[i, j];
-                    Console.Write($"{table[i,j],2} ");
-                }
-                Console.WriteLine($": {mult}");
-                Console.WriteLine(table.Rank);
-            }
+            
             
         }
     }
