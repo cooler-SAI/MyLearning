3759386f040d99dafada9f3822b7c90569b9bc47
cooler-SAI <cooler19860212@yahoo.com>
2022-04-15 21:15:36 +0300

    Code: some array task test

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,7 +6,22 @@ namespace Study
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
 
+            int[,] table = new int[4, 4];
+
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < table.GetLength(1); j++)
+                {
+                    table[i, j] = rand.Next(1, 10);
+                    sum += table[i, j];
+                    Console.Write($"{table[i,j],2} ");
+                }
+                Console.WriteLine($": {sum}");
+            }
             
         }
     }
