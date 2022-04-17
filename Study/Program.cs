fa7de4de183b3f61815bf57dd1c7ffc60a78adcf
cooler-SAI <cooler19860212@yahoo.com>
2022-04-17 15:25:55 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,22 +6,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter height of your Triangle:");
-            int heightTriagle = int.Parse(Console.ReadLine());                     
-
-            for (int i = 0; i < heightTriagle; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (int j = heightTriagle; j > i; j--)
-                {
-                    Console.Write("#");
-                }
-                Console.WriteLine();
-                
-            }
+            
         }
 
     }
