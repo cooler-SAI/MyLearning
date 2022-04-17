880ddc83e6a94131822ce32c1501c8baa0d8bd8a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-17 15:51:52 +0300

    Code: numb

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,12 +6,8 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number:");
-            int inputData = int.Parse(Console.ReadLine());
-            int outputData = (inputData < 0) ? 0 : inputData;
-
-            Console.WriteLine(outputData);
-
+            // 23
+           
             
         }
 
