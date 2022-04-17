c689cdee2e84f408c0054a6707d8b8234bc54ee5
cooler-SAI <cooler19860212@yahoo.com>
2022-04-17 16:11:18 +0300

    Code: arrays

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,6 +7,12 @@ namespace Study
         static void Main(string[] args)
         {
             // 23
+            int[] myArray = new int[5];
+
+            int[] myArray2 = new int[5] { 1, 2, 3, 4, 5 };
+            int[] myArray3 = new int[] { 1, 2, 3, 4, 5, 6 };
+            
+
            
             
         }
