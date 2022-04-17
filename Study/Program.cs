1cef3ad45b74735f2c74d3c78788ca383d91a12a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-17 13:45:41 +0300

    Code: added triangle to console

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,9 +5,20 @@ namespace Study
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
+            Console.WriteLine("Please enter height of your Triangle:");
+            int heightTriagle = int.Parse(Console.ReadLine());
 
-            
+
+            for (int i = 0; i < heightTriagle; i++)
+            {
+                for (int j = heightTriagle; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
         }
 
     }
