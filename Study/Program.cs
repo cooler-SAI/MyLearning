1a746c521e4b7dc40096c60d7eae8ebcde69349a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-17 15:42:46 +0300

    Code: operator test

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,6 +6,24 @@ namespace Study
     {
         static void Main(string[] args)
         {
+            bool allowedPassword;
+
+            Console.WriteLine("Enter the password, please:");
+            string enteredPassword = Console.ReadLine();
+            string storedPassword = "qwerty";
+
+            //if (enteredPassword == storedPassword)
+            //{
+            //    allowedPassword = true;
+            //}
+            //else
+            //{
+            //    allowedPassword = false;
+            //}
+
+            allowedPassword = enteredPassword == storedPassword ? true : false;
+            Console.WriteLine(allowedPassword);
+
             
         }
 
