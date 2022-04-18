b59c674644b5e57089eafd44e6c2101e7c72222d
cooler-SAI <cooler19860212@yahoo.com>
2022-04-18 14:06:56 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,13 +6,7 @@ namespace Study
     class Program
     {
         static void Main(string[] args)
-        {            // 27
-
-            int[] myArray = { 20, 13, 22, 2 };
-
-            int[] result = myArray.OrderByDescending(e => e).ToArray();
-
-            Array.Reverse(myArray);
+        {            // 28
             
         }
 
