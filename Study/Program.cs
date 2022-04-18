48edb250e4a08df2c29d68a6173c641f0580389a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-18 11:10:38 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,21 +8,7 @@ namespace Study
         static void Main(string[] args)
         {
             // 26
-            int a = 5;
-            int b = 10;
             
-
-            //if (a > b)
-            //{
-            //    c = true;
-            //}
-            //else
-            //{
-            //    c = false;
-            //}
-
-            bool c = (a > b) ? true : false;
-            Console.WriteLine(c);
            
             
         }
