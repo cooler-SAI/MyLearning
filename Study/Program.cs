5cbefde8119030cdcc54d2fbd79bc59308f65b7f
cooler-SAI <cooler19860212@yahoo.com>
2022-04-18 13:20:22 +0300

    Code: more practic here

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,32 +8,18 @@ namespace Study
         static void Main(string[] args)
         {
             // 27
-            Console.WriteLine("Enter the lenght of youe array:");
-            int lenghtNum;
-            int.TryParse(Console.ReadLine(),out lenghtNum);
-
-            Random rand = new Random();
-            int[] myArray = new int[lenghtNum];
-
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                myArray[i] = rand.Next(10);
-            }
-            for (int i = (myArray.Length -1); i >= 0; i--)
-            {
-                Console.Write(" " + myArray[i]);
-            }
+            int[] myArray = { 10, 15, 42, 80, 2 };
 
             int minValue = myArray[0];
+
             for (int i = 0; i < myArray.Length; i++)
             {
-                if (myArray[i] < myArray[0])
+                if (myArray[i] < minValue)
                 {
                     minValue = myArray[i];
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine($"Your min value is: {minValue} ");
+            Console.WriteLine($"Minimun element value is {minValue}");
         }
 
     }
