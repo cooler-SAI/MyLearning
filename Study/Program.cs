91ce2c2d02a7934f2b459c76082a9693bea0171e
cooler-SAI <cooler19860212@yahoo.com>
2022-04-18 12:27:44 +0300

    Code: more and more practic

--- Study/Program.cs
+++ Study/Program.cs
@@ -17,7 +17,8 @@ namespace Study
 
             for (int i = 0; i < myArray.Length; i++)
             {
-                myArray[i] = rand.Next(10);
+                Console.WriteLine($"Please enter {i} element:");
+                myArray[i] = int.Parse(Console.ReadLine());
             }
             for (int i = (myArray.Length -1); i >= 0; i--)
             {
