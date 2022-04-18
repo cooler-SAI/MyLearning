6d6715cef2474c08587a725cb93ad1ca29b8d22e
cooler-SAI <cooler19860212@yahoo.com>
2022-04-18 11:19:48 +0300

    Code: logic test

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,7 +8,13 @@ namespace Study
         static void Main(string[] args)
         {
             // 26
-            
+
+            int[] myArray = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
            
             
         }
