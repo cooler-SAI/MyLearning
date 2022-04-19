aeb233a0288a7b33b3c51dbeb945453d294e52b6
cooler-SAI <cooler19860212@yahoo.com>
2022-04-19 23:36:52 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,35 +7,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter height of your Array:");
-            int height = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please Enter width of your Array:");
-            int width = int.Parse(Console.ReadLine());
-
-            int [,] myArray = new int [height, width];
-            Random rand = new Random();
-
-            for (int i = 0; i < height; i++)
-            {
-                               
-                for (int j = 0; j < width; j++)
-                {
-                    Console.WriteLine($"Please Enter value height:{i} width:{j}");
-                    myArray[i, j] = int.Parse(Console.ReadLine());                    
-                }
-                
-            }
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    Console.Write($"{myArray[i, j]} ");
-                }
-                Console.WriteLine();
-            }
-
-
+            //33
 
         }
 
