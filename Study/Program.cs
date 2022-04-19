7e12cce403e1b4393b6950510cfb4212034509ed
cooler-SAI <cooler19860212@yahoo.com>
2022-04-19 23:22:12 +0300

    Code: test up 2D filled Array

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,10 +7,28 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            
-            
+            Console.WriteLine("Please Enter height of your Array:");
+            int height = int.Parse(Console.ReadLine());
 
-            
+            Console.WriteLine("Please Enter width of your Array:");
+            int width = int.Parse(Console.ReadLine());
+
+            int [,] myArray = new int [height, width];
+            Random rand = new Random();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    myArray[i, j] = rand.Next(5, 10);
+
+                    Console.Write($"{myArray[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+
         }
 
     }}
