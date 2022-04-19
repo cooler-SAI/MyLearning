1b8b2ce79142671389525ecb9af56dc1df038779
cooler-SAI <cooler19860212@yahoo.com>
2022-04-19 23:13:13 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,27 +7,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            int[,] myArray = 
-            {
-                {1,2,4,4,4 },
-                {12,41,62,4,5 },
-                {42,12,10,74,10 },
-                {42,12,10,74,10 }
-
-            };
-
-            int height = myArray.GetLength(0);
-            int width = myArray.GetLength(1);
-
-            for (int h = 0; h < height; h++)
-            {
-                for (int w = 0; w < width; w++)
-                {
-                    Console.Write(myArray[h,w] + "\t");
-
-                }
-                Console.WriteLine();
-            }
+            
             
 
             
