3195adc2f3dc94dd6d1ffd4ad021e146d4f0feb3
cooler-SAI <cooler19860212@yahoo.com>
2022-04-19 22:31:44 +0300

    Code: test 2d array

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,8 +6,8 @@ namespace Study
     class Program
     {
         static void Main(string[] args)
-        {            
-            
+        {
+            int[,] myArray = new int[3, 5];
         }
 
     }}
