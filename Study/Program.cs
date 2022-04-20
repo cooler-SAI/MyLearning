60275c64611da751c6e846c9137784c38f05c60a
cooler-SAI <cooler19860212@yahoo.com>
2022-04-20 12:20:51 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,34 +7,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            int[][] myArray = new int[8][];
-            myArray[0] = new int[5];
-            myArray[1] = new int[10];
-            myArray[2] = new int[15];
-            myArray[3] = new int[13];
-            myArray[4] = new int[2];
-            myArray[5] = new int[8];
-            myArray[6] = new int[10];
-            myArray[7] = new int[22];
-
-            Random rand = new Random();
-
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                for (int j = 0; j < myArray[i].Length; j++)
-                {
-                    myArray[i][j] = rand.Next(100);
-                }
-            }
-
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                for (int j = 0; j < myArray[i].Length; j++)
-                {
-                    Console.Write($"{myArray[i][j]}  ");
-                }
-                Console.WriteLine();
-            }
+            
         }
 
     }}
