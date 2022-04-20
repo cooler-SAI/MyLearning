d36305d88303cf0336ad06a8b3f7618ddf97a85f
cooler-SAI <cooler19860212@yahoo.com>
2022-04-20 19:23:57 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,20 +5,14 @@ namespace Study
 {
     class Program
     {
-        static int Sum (int a, int b)
-        {
-            return  a + b;
-        }
 
         static void Main(string[] args)
         {
-            int a, b;
 
-            a = int.Parse(Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
 
-            Sum(a, b);
-            Console.WriteLine(Sum(a, b));
         }
 
-    }}
+    }
+
+}
+
