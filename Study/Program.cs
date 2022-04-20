e30bb1ea45631330079a5b5faea1c5c8ca98386c
cooler-SAI <cooler19860212@yahoo.com>
2022-04-20 13:30:31 +0300

    Code: added function

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,9 +5,23 @@ namespace Study
 {
     class Program
     {
+        static int Sum(int a, int b)
+        {
+            int result = a + b;
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
-           
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);
+
+            Console.WriteLine(c);
         }
 
     }}
