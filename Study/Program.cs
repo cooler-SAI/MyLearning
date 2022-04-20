dfc0af4384361eefec7ca658bf9f9ca90a9d4fbc
cooler-SAI <cooler19860212@yahoo.com>
2022-04-20 12:07:39 +0300

    Code: added func for 2x array

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,8 +7,14 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            int[][] myArray = new int[5][];
+            int[][] myArray = new int[3][];
             myArray[0] = new int[5];
+            myArray[1] = new int[10];
+            myArray[2] = new int[15];
+
+            myArray[1][9] = 55;
+
+            Console.WriteLine(myArray[1][9]);
         }
 
     }}
