faa83e907c9718def93ecca794135d6e4d24c4c6
cooler-SAI <cooler19860212@yahoo.com>
2022-04-20 12:59:02 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,33 +7,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            int[,,] myArray = new int[4, 3, 5];
-
-            Random rand = new Random();
-
-            for (int i = 0; i < myArray.GetLength(0); i++)
-            {
-                for (int j = 0; j < myArray.GetLength(1); j++)
-                {
-                    for (int k = 0; k < myArray.GetLength(2); k++)
-                    {
-                        myArray[i, j, k] = rand.Next(100);
-                    }
-                }
-            }
-
-            for (int i = 0; i < myArray.GetLength(0); i++)
-            {
-                Console.WriteLine($"######### Page {i}:");
-                for (int j = 0; j < myArray.GetLength(1); j++)
-                {
-                    for (int k = 0; k < myArray.GetLength(2); k++)
-                    {
-                        Console.Write($"{myArray[i, j, k]} ");
-                    }
-                    Console.WriteLine();
-                }
-            }
+           
         }
 
     }}
