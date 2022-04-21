369bd320cd57b51ebe49a173840d2d44d71f9a99
cooler-SAI <cooler19860212@yahoo.com>
2022-04-21 23:35:22 +0300

    Code: added func new

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Linq;
 
+
 namespace Study
 {
     class Program
     {
-        
+        static void PrintLine (string symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
         static void Main(string[] args)
         {
-           // Homework!
+            // Homework!
+            PrintLine("#", 10);
+
+            Console.ReadKey();
+
+
+
+
         }
 
     }
