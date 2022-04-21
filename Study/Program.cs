7f8fcfad1b59ae001c7a616fb5f139b50b4fc340
cooler-SAI <cooler19860212@yahoo.com>
2022-04-21 23:10:06 +0300

    Code: added func for test

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,6 +5,10 @@ namespace Study
 {
     class Program
     {
+        static int Sum(int a,int b)
+        {
+            return a + b;
+        }
 
         static void Main(string[] args)
         {
