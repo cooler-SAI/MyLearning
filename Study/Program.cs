e4f58b9a7ad8ea043eb049324d19dfc9786b1f96
cooler-SAI <cooler19860212@yahoo.com>
2022-04-21 23:48:34 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,28 +6,10 @@ namespace Study
 {
     class Program
     {
-        static void PrintLine (char symbol, uint symbolsCount)
-        {
-            for (int i = 0; i < symbolsCount; i++)
-            {
-                Console.Write(symbol);
-            }
-        }
+        
         static void Main(string[] args)
         {
-            // Homework!
-            Console.Write("Please enter the symbol:");
-            char symbol = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            Console.Write("Please enter Number of Symbols:");
-            uint symbolsCount = uint.Parse(Console.ReadLine());
-
-
-            PrintLine(symbol, symbolsCount);
-
-            Console.ReadKey();
-
-
+            
 
 
         }
