6710d8991161df94be01560f4d1e38c895d51514
cooler-SAI <cooler19860212@yahoo.com>
2022-04-21 23:29:05 +0300

    Code: clearn up, added homework

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,31 +5,10 @@ namespace Study
 {
     class Program
     {
-        static int Result(int a,int b)
-        {
-            return a * b;
-        }
-
-        static void Info()
-        {
-            Console.WriteLine("Result Here!");
-        }
-
+        
         static void Main(string[] args)
         {
-            int a = 10;
-            int b = 10;
-
-
-            int c = Result(a, b);
-
-
-            Info();
-
-
-            Console.WriteLine(c);
-
-
+           // Homework!
         }
 
     }
