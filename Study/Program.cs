9b2751150f4055a69f2441898c3eac9ec41e1116
cooler-SAI <cooler19860212@yahoo.com>
2022-04-21 23:41:05 +0300

    Code: more added

--- Study/Program.cs
+++ Study/Program.cs
@@ -16,7 +16,13 @@ namespace Study
         static void Main(string[] args)
         {
             // Homework!
-            PrintLine("#", 10);
+            Console.WriteLine("Please enter the symbol:");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("Please enter Number of symbols:");
+            uint symbolsCount = uint.Parse(Console.ReadLine());
+
+
+            PrintLine(symbol, symbolsCount);
 
             Console.ReadKey();
 
