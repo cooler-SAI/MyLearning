9eb7aa03c8a4ee7558f5b341992c16b825800004
cooler-SAI <cooler19860212@yahoo.com>
2022-04-22 22:34:44 +0300

    Code: added method

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,10 +6,14 @@ namespace Study
 {
     class Program
     {
-        
+        static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
         static void Main(string[] args)
         {
-            
+            Hello();
 
 
         }
