2febd3c132f12188fb0e62938c621b3a706a117b
cooler-SAI <cooler19860212@yahoo.com>
2022-04-23 19:52:08 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,27 +7,10 @@ namespace Study
 {
     class Program
     {
-        static void Hello()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-         static void PauseMe()
-        {
-            Console.ReadLine();
-        }
-
-        static void Pause (int Interval)
-        {
-            Thread.Sleep(Interval * 1000);
-        }
-
-
+        
         static void Main(string[] args)
         {
-            Hello();
-            PauseMe();
-            PauseMe();
+            
 
         }
 
