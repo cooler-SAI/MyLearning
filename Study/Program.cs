0e144a2262b436b184b94e70db9a5e525482f915
cooler-SAI <cooler19860212@yahoo.com>
2022-04-24 23:44:07 +0300

    Code: few tests here

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,10 +7,30 @@ namespace Study
 {
     class Program
     {
-        
+        static int NumResult(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void ConsoleMessage()
+        {
+            System.Console.WriteLine("Hello to all here!");
+            
+        }
+                
         static void Main(string[] args)
         {
-            
+            int a = 10;
+            int b = 20;
+            int c = NumResult(a, b);
+
+            Console.WriteLine(c);
+
+            ConsoleMessage();
+
+
+
+
 
         }
 
