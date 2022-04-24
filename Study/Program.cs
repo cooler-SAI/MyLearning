ce3cbad33b6670aa152414f39ca9a2abff65bd11
cooler-SAI <cooler19860212@yahoo.com>
2022-04-24 13:12:34 +0300

    Code: clearn up all

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,33 +7,10 @@ namespace Study
 {
     class Program
     {
-        //Task 1:
-        static void WelcomeMessage()
-        {
-            Console.WriteLine("Please Enter few words here:");
-        }
-
-        static string[] WordSplitter(string UserString)
-        {
-            string[] UserSentence = UserString.Split(' ');
-            foreach (var chars in UserSentence)
-            {
-                Console.WriteLine($"Substring: {chars}");
-            }
-            return UserSentence;
-        }
-
+        
         static void Main(string[] args)
         {
-            //Task 1:
-            WelcomeMessage();
-            string UserString = Console.ReadLine();
-            WordSplitter(UserString);
-
-
-
-
-
+           
         }
 
     }
