7f1f9bc2593606ef408288d2171a186f76bb9a32
cooler-SAI <cooler19860212@yahoo.com>
2022-04-25 18:27:27 +0300

    Code: added heavy methods

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,10 +7,24 @@ namespace Study
 {
     class Program
     {
-                        
+        static int Result( int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Result(int num1, int num2,int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
         static void Main(string[] args)
         {
-            
+            int num1 = 20;
+            int num2 = 10;
+            int num3 = 30;
+            int result = Result(num1, num2);
+
+            Console.WriteLine(result);
+
 
         }
 
