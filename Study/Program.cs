b4b8465948d6f6fc681748f7d8e70f210778416f
cooler-SAI <cooler19860212@yahoo.com>
2022-04-25 15:26:54 +0300

    Code: more here

--- Study/Program.cs
+++ Study/Program.cs
@@ -11,10 +11,18 @@ namespace Study
         {
             Console.ReadKey();
         }
+
+        static double UserNumber (double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        
         static void Main(string[] args)
         {
             Pause();
-
+            double num1 = 5;
+            double num2 = 5;
+            Console.WriteLine(UserNumber(num1, num2)); 
         }
 
     }
