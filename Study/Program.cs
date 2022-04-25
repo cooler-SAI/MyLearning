817ce8041ad0dad071c9a4ded84e4978bdfeeae5
cooler-SAI <cooler19860212@yahoo.com>
2022-04-25 15:16:48 +0300

    Code: more tests

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,10 +7,13 @@ namespace Study
 {
     class Program
     {
-        
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
         static void Main(string[] args)
-        {          
-
+        {
+            Pause();
 
         }
 
