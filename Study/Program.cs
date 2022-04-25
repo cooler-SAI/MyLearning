2071aa8c98c89f9874b7e93ed99cef63a34da507
cooler-SAI <cooler19860212@yahoo.com>
2022-04-25 18:11:50 +0300

    Code: more data added

--- Study/Program.cs
+++ Study/Program.cs
@@ -18,12 +18,24 @@ namespace Study
             }
             return -1;
         }
+        static int[] GetRandomArray (uint Length, int minValue, int maxValue)
+        {
+            Random rand = new Random();
+            int[] UserArray = new int[Length];
+
+            for (int i = 0; i < UserArray.Length; i++)
+            {
+                UserArray[i] = rand.Next(minValue, maxValue);
+            }
+            return UserArray;
+
+        }
             
         
                 
         static void Main(string[] args)
         {
-            int[] myArray = { 22, 15, 16, 21, 30 };
+            int[] myArray = GetRandomArray(10, -20, 10);
             int result = IndexOf(myArray, 21);
 
             Console.WriteLine($" Element, that has this value is: {result} ");
