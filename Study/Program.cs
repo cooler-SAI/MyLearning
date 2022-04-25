2ed0e0a9ffbcea04493b05264fab7f009ab72c9b
cooler-SAI <cooler19860212@yahoo.com>
2022-04-25 18:12:46 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,39 +7,10 @@ namespace Study
 {
     class Program
     {
-        static int IndexOf( int[] UserArray, int num)
-        {
-            for (int i = 0; i < UserArray.Length; i++)
-            {
-                if (UserArray[i] == num)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-        static int[] GetRandomArray (uint Length, int minValue, int maxValue)
-        {
-            Random rand = new Random();
-            int[] UserArray = new int[Length];
-
-            for (int i = 0; i < UserArray.Length; i++)
-            {
-                UserArray[i] = rand.Next(minValue, maxValue);
-            }
-            return UserArray;
-
-        }
-            
-        
-                
+                        
         static void Main(string[] args)
         {
-            int[] myArray = GetRandomArray(10, -20, 10);
-            int result = IndexOf(myArray, 21);
-
-            Console.WriteLine($" Element, that has this value is: {result} ");
-
+            
 
         }
 
