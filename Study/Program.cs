bf67e1ec17e17cd455b5c7f40b377e177e384314
cooler-SAI <cooler19860212@yahoo.com>
2022-04-25 19:18:23 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,31 +7,11 @@ namespace Study
 {
     class Program
     {
-        /// <summary>
-        /// Summ of 2 numbers
-        /// </summary>
-        /// <param name="num1"></param>
-        /// <param name="num2"></param>
-        /// <returns></returns>
-        static int Result( int num1, int num2)
-        {
-            return num1 + num2;
-        }
-        static int Result(int num1, int num2,int num3)
-        {
-            return num1 + num2 + num3;
-        }
+        
 
         static void Main(string[] args)
         {
-            int num1 = 20;
-            int num2 = 10;
-            int num3 = 30;
-            int result = Result(num1, num2, num3);
-
-            Console.WriteLine(result);
-
-
+            
         }
 
     }
