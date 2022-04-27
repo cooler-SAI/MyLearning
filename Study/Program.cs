15939fbd11c0cc192b756c67dad2ca66146db71b
cooler-SAI <cooler19860212@yahoo.com>
2022-04-27 20:42:00 +0300

    Code: added method string

--- Study/Program.cs
+++ Study/Program.cs
@@ -6,12 +6,15 @@ using System.Threading;
 namespace Study
 {
     class Program
-    {
-        
+    {      
+        static void Text(string Text)
+        {
+            Console.WriteLine();
+        }
 
         static void Main(string[] args)
         {
-            
+            Text("Hi all here!");
         }
 
     }
