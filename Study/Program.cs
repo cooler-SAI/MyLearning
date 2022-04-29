4df0b03ad771cc221979313d6d5d2462b2db76f7
cooler-SAI <cooler19860212@yahoo.com>
2022-04-29 18:16:11 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,38 +7,9 @@ namespace Study
 {
     class Program
     {      
-        static void Text(string Text)
-        {
-            Console.WriteLine(Text);
-        }
-
-        static int NumResult(int num1, int num2)
-        {
-            return num1 * num2; 
-        }
-
+        
         static void Main(string[] args)
-        {
-            Text("Hi all here!");
-            Console.WriteLine("Hi guys! ");
-
-            Console.WriteLine("Please enter Num 1: ");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(NumResult(num1,num2));
-
-            int[] myArray = { 12, 22, 45, 16 };
-            int result = 0;
-            foreach (var e in myArray)
-            {
-                result = e + 100;
-            }
-            Console.WriteLine(result);
-
-            int[,] myArray2 = new int[10, 12];
+        {            
 
             
         }
