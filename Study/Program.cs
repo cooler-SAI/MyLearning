ab62787902286386b99a7c42bb80e742c2d81a6f
cooler-SAI <cooler19860212@yahoo.com>
2022-05-01 14:15:55 +0300

    Code: test one

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,18 +7,8 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            // 26
-            Console.WriteLine("Hello! enter how many elements will be in Array:");
-            int elements;
-            int.TryParse(Console.ReadLine(), out elements);
 
-            int[] myArray = Enumerable.Range(1, elements).ToArray();
-
-            for (int i = 0; i < elements; i++)
-            {
-                Console.WriteLine(myArray[i]);
-            }
-            
+            Console.WriteLine(Math.Round(3.25,20));
 
            
             
