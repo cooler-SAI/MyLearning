f9c059d11d3b3282f729af57face22aadda3ca5a
cooler-SAI <cooler19860212@yahoo.com>
2022-05-08 00:38:11 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,24 +8,11 @@ using System.Threading.Tasks;
 namespace Study
 {
     class Program
-    {
-        
+    {        
         static void Main(string[] args)
         {
-            int[] myArray = new int[10];
 
-            int value = 10;
-
-            for (int e = 0; e < myArray.Length; e++)
-            {
-                myArray[e] = value++;
-                Console.WriteLine(myArray);
-            }
-
-
-
-
-
+            
         }
     }
 }
