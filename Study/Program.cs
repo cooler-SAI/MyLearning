c3984c442d82a26828f1c85d83d19a26d530e2c7
cooler-SAI <cooler19860212@yahoo.com>
2022-05-08 01:15:47 +0300

    Core: added Main menu of Game

--- Study/Program.cs
+++ Study/Program.cs
@@ -8,12 +8,45 @@ using System.Threading.Tasks;
 namespace Study
 {
     class Program
-    {        
+    {    
+        static void WelcomeRaceInfo()
+        {
+            Console.WriteLine("You can Choose Race Here:\n" +
+                "\n1 - Humans = Glory Race"+
+                "\n2 - Dwarfs = Glory Race"+
+                "\n3 - Elves = Glory Race"
+                );
+            
+        }
         static void Main(string[] args)
         {
             Empire Faction1 = new Empire();
+            Faction1.Humans = "Glory Race";
+            Faction1.Dwarfs = "strong Race";
+            Faction1.HighElves = "Older race";
+            Console.WriteLine();
+            WelcomeRaceInfo();
+            Console.WriteLine($"Hello, Player! Please choose your character class: 1, 2 or 3");
+            string PlayerChoise = Console.ReadLine();
+                       
 
-            
+            switch (PlayerChoise)
+            {
+                case "1":
+                    Console.WriteLine("You chose Human!");
+                    break;
+                case "2":
+                    Console.WriteLine("You chose Dwarf!");
+                    break;
+                case "3":
+                    Console.WriteLine("You chose HighElve!");
+                    break;
+                default:
+                    Console.WriteLine("Please made a choise again!");
+                    break;
+            }
+            Console.ReadKey(); 
+
         }
     }
 }
