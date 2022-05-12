410e375b53170dd3fa95669129ff64713371fb94
cooler-SAI <cooler19860212@yahoo.com>
2022-05-12 16:50:19 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -9,41 +9,8 @@ namespace Study
 {
     class Program
     {    
-        static void WelcomeRaceInfo()
-        {
-            Console.WriteLine("You can Choose Race Here:\n" +
-                "\n1 - Humans = Glory Race"+
-                "\n2 - Dwarfs = Glory Race"+
-                "\n3 - Elves = Glory Race"
-                );            
-        }
         static void Main(string[] args)
-        {
-            Empire Faction1 = new Empire("Glory Race", "Strong Dwarfs", "Mighty Race");
-            Console.WriteLine(Faction1.Info());
-
-            Console.WriteLine();
-            WelcomeRaceInfo();
-            Console.WriteLine($"Hello, Player! Please choose your character class: 1, 2 or 3");
-            string PlayerChoise = Console.ReadLine();
-                       
-
-            switch (PlayerChoise)
-            {
-                case "1":
-                    Console.WriteLine("You chose Human!");
-                    break;
-                case "2":
-                    Console.WriteLine("You chose Dwarf!");
-                    break;
-                case "3":
-                    Console.WriteLine("You chose HighElve!");
-                    break;
-                default:
-                    Console.WriteLine("Please made a choise again!");
-                    break;
-            }
-            Console.ReadKey(); 
+        {            
 
         }
     }
