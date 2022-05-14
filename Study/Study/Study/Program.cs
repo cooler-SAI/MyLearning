afe7e60c9f0f7f86896b8aa5bb8882ce32234065
cooler-SAI <cooler19860212@yahoo.com>
2022-05-14 22:10:10 +0300

    Core: test InvariantCulture

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿double number2;
+number2 = double.Parse("34.42", CultureInfo.InvariantCulture);
+string invariantNumber2 = number2.ToString(CultureInfo.InvariantCulture);
+Console.WriteLine(invariantNumber2);
