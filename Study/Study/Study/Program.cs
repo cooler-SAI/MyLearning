027eeb4e6bf4c109bd695b8932a4187177885bed
cooler-SAI <cooler19860212@yahoo.com>
2022-05-15 23:23:39 +0300

    Code: test List<> at work!))

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -1,44 +1,19 @@
 ﻿namespace Study
 {
     class Program
-    {
-        /// <summary>
-        /// Round double number
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        static int DivideandRound(double a, double b)
-        {
-            return (int)Math.Round(a / b);
-        }
-        private static string GetGreetingMessage(string name, double salary)
-        {
-            return $"Hello, {name}, your salary is {Math.Ceiling(salary)}";
-
-        }
-        static void Print(int number)
-        {
-            Console.WriteLine(number);
-        }
-        static int GetSquare(int number)
-        {
-            return (int)Math.Pow(number, 2);
-        }
-        static string GetLastHalf(string text)
-        {
-            return (text).Substring(text.Length / 2);
-        }        
+    {             
         static void Main(string[] args)
         {
-            Console.WriteLine(DivideandRound(8, 2));
-
-            Console.WriteLine(GetGreetingMessage("Ander", 12000));
-
-            Print(GetSquare(42));
+            List<string> list = new List<string>();
+            list.Add("Elves");
+            list.Add("Gnomes");
+            list.Add("Orcs");
+            foreach (string item in list)
+            {
+                Console.WriteLine($"\nRace is: {item} ");
+            }
 
         }
-
         
     }
   
