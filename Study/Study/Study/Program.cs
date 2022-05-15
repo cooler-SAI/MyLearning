ad4aa41596242f463b3ed36aa9955f3538c5e5dd
cooler-SAI <cooler19860212@yahoo.com>
2022-05-15 20:36:11 +0300

    Code: more methods test

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -2,15 +2,26 @@
 {
     class Program
     {
+        /// <summary>
+        /// Round double number
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         static int DivideandRound(double a, double b)
         {
             return (int)Math.Round(a / b);
+        }
+        private static string GetGreetingMessage(string name, double salary)
+        {
+            return $"Hello, {name}, your salary is {Math.Ceiling(salary)}";
+
         }
         static void Main(string[] args)
         {
             Console.WriteLine(DivideandRound(8, 2));
 
-            Console.WriteLine();
+            Console.WriteLine(GetGreetingMessage("Ander", 12000));
         }
     }
 }
