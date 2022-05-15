1d29d08f84ebcb92e8f99a10d69f626acf0e4e18
cooler-SAI <cooler19860212@yahoo.com>
2022-05-15 20:47:45 +0300

    Code: more tests with Methods

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -17,12 +17,24 @@
             return $"Hello, {name}, your salary is {Math.Ceiling(salary)}";
 
         }
+        static void Print(int number)
+        {
+            Console.WriteLine(number);
+        }
+        static int GetSquare(int number)
+        {
+            return (int)Math.Pow(number, 2);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine(DivideandRound(8, 2));
 
             Console.WriteLine(GetGreetingMessage("Ander", 12000));
+
+            Print(GetSquare(42));
         }
+
+        
     }
 }
 
