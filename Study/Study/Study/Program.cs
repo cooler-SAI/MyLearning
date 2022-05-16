ab05d272ad0138737023941555ca9d2bfbf7d0bf
cooler-SAI <cooler19860212@yahoo.com>
2022-05-16 10:09:36 +0300

    Code: clearn up

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -4,26 +4,7 @@
     {             
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
-            list.Add("Elves");
-            list.Add("Gnomes");
-            list.Add("Orcs");
-
-            list.Insert(1, "Humans");
-            list.Insert(1, "Humans");
-
-            list.Remove(list[1]);
-
-            list.Sort();
-
-            list.Add("Orcs");
-            list.RemoveAt(4);
-
-            foreach (string item in list)
-            {
-                Console.WriteLine($"\nRace is: {item} ");
-            }
-            Console.ReadLine();
+            
         }
         
     }
