db6e94c6fa1351078488aa6dc1bc971fbb5c9ba0
cooler-SAI <cooler19860212@yahoo.com>
2022-05-16 11:18:37 +0300

    Code: clearn

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -4,26 +4,8 @@
     {             
         static void Main(string[] args)
         {
-            List<Worker> workers = new List<Worker>();
-            Random random = new Random();
-
-            for (int i = 1; i < 10; i++)
-            {
-                workers.Add(
-                    new Worker(
-                        (int)random.Next(0,10),
-                        DateTime.Now,
-                        $"Worker Fullname_{i}",
-                        random.Next(19,44),
-                        random.Next(170, 182),
-                        DateTime.Now,
-                        $"Отдел_{i}"));
-            }
-
-            foreach (var worker in workers)
-            {
-                Console.WriteLine(worker);
-            }
+            
+            
         }
         
     }
