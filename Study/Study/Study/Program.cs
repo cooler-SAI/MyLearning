7521c2e39911146b447a6dee2e6cab4fd693cbf4
cooler-SAI <cooler19860212@yahoo.com>
2022-05-16 12:38:59 +0300

    Code: clearn up

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -4,24 +4,8 @@
     {             
         static void Main(string[] args)
         {
-            List<Worker> workers = new List<Worker>();
-            Random random = new Random();
+            
 
-            for (int i = 1; i < 10; i++)
-            {
-                workers.Add(
-                    new Worker(
-                        $"Имя_{i}",
-                        $"Фамилия_{i}",
-                        $"Должность_{i}",
-                        (uint)random.Next(1000, 2000),
-                        $"Отдел_{i}"));
-            }
-
-            foreach (var worker in workers)
-            {
-                Console.WriteLine(worker.Print());
-            }
         }
         
     }
