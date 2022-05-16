32cefd2afc430e028615fe9f399330c6652a4622
cooler-SAI <cooler19860212@yahoo.com>
2022-05-16 14:59:29 +0300

    Code: here and there test up Dictionary!

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Study
 {
     class Program
-    {             
+    {
+        
         static void Main(string[] args)
         {
-            
+           Dictionary<string, string> russian = new Dictionary<string, string>();
+
+            russian.Add("Hello", "Привет");
+            russian.Add("Bye", "ПОка");
+
+            foreach (var item in russian)
+            {
+                Console.WriteLine(item);
+            }
+           
+
+
+
 
         }
         
