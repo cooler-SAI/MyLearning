f1cf4bc1ba9443d82cbe63ce87a72e7638383931
cooler-SAI <cooler19860212@yahoo.com>
2022-05-16 20:36:03 +0300

    homework: added Task_1

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,62 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Study
 {
-    class Program
-    {
-        static void Main(string[] args)
+    public class Program
+    {        
+        public static void Main(string[] args)
         {
+            //////////////////////////////////////////////////////////////////////
             // Task 1
-            string path = @"data.csv";
-            DataStore datastore = new DataStore(path);            
-            while (true)
-            {
-                datastore.WelcomeMessage();
-                Console.WriteLine(
-                "Please Enter the Number:" +
-                "\n" +
-                "\n1: Show Worker by ID" +
-                "\n2: Add new Worker Data " +
-                "\n3: Delete Worker Data " +
-                "\n4: Edit Worker Data " +
-                "\n5: Show Worker Data by period" +
-                "\n6: Show Worker Data by descending" +
-                "\n7: Show Worker Data by ascending" +
-                "\n8: About Program ");
-                int UserChoise;
-                int.TryParse(Console.ReadLine(), out UserChoise);                
+            WorkingRoom workingroom = new WorkingRoom(); // link Methods here
+            List<int> numbers = new List<int>(); // create List of numbers
 
-                switch (UserChoise)
-                {
-                    case 1: datastore.ShowIDWorker();
-                        break;
-                    case 2: datastore.AddWorkerData();                        
-                        break;
-                    case 3: datastore.DeleteWorkerData();                        
-                        break;
-                    case 4: datastore.EditWorkerData();
-                        break;
-                    case 5: datastore.PeriodWorkerData();
-                        break;
-                    case 6: datastore.DescendingWorkerData();
-                        break;
-                    case 7: datastore.AscendingWorkerData();
-                        break;                    
-                    case 8:
-                        Console.WriteLine(" Program Created by Ander (cooler-SAI)" +
-                            "\n v 1.2 (12.05.2022) Stavropol City, Russia" +
-                            "\n" +
-                            "\n");
+            workingroom.FillList(numbers); // Random fill List of numbers
+            workingroom.ShowList(numbers); // Show List in Console
+            workingroom.DeleteRangeList(numbers); // Delete Range numbers in Console
+            workingroom.ShowList(numbers); // Show List in Console
 
-                        Console.ReadKey();
-                        break;
-                    default:
-                        break;
+            Console.ReadKey();
+            //////////////////////////////////////////////////////////////////////
+            
+            
+            
 
-                }
-                datastore.Save();
-
-            }
+            
             
 
 
---
+++ Study/WorkingRoom.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Study
+{
+    public class WorkingRoom
+    {
+        //////////////////////////////////////////////////////////////////////
+        // Task 1
+        #region Methods
+        /// <summary>
+        /// Random fill List of numbers
+        /// </summary>
+        public void FillList(List<int> list)
+        {
+            Random random = new Random();
+            int listSize = 100; // we added List size here
+            int count = 0; // we set numbers counter here
+
+            for (int i = 0; i<listSize; i++) 
+            {
+                list.Add(random.Next(0, 100)); // we fill List by Random numbers here
+                count++; 
+            }
+            Console.WriteLine($"You have {count} numbers here: "); // we place number counter to console
+        }
+        /// <summary>
+        /// Show List in Console
+        /// </summary>
+        /// <param name="list"></param>
+        public void ShowList(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($" {list[i]} "); // print all numbers to console
+            }
+        }
+        /// <summary>
+        /// Delete Range numbers in Console
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public List<int> DeleteRangeList(List<int> list)
+        {
+            int MinNumber = 25; // we set min here for start range of numbers
+            int MaxNumber = 50; // we set max here for end range of numbers
+            int count = 0; // we set numbers counter here
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                count++;
+                if (list[i] >= MinNumber && list[i] <= MaxNumber)
+                {
+                    list.RemoveAt(i); // we delete numbers here
+                                     
+                }                               
+            }
+            Console.WriteLine($"\nYou have {count} numbers here: "); // we place number counter to console
+            return list;            
+        }
+        #endregion
+        //////////////////////////////////////////////////////////////////////
+
+
+
+
+
+    }
+}
