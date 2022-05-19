62a40d77573bfa41d28876be1625852892de2316
cooler-SAI <cooler19860212@yahoo.com>
2022-05-19 14:30:10 +0300

    homework: added Task_2

--- Study/Program.cs
+++ Study/Program.cs
@@ -9,17 +9,58 @@ namespace Study
         {
             //////////////////////////////////////////////////////////////////////
             // Task 1
-            WorkingRoom workingroom = new WorkingRoom(); // link Methods here
-            List<int> numbers = new List<int>(); // create List of numbers
+            //WorkingRoom workingroom = new WorkingRoom(); // link Methods here
+            //List<int> numbers = new List<int>(); // create List of numbers
 
-            workingroom.FillList(numbers); // Random fill List of numbers
-            workingroom.ShowList(numbers); // Show List in Console
-            workingroom.DeleteRangeList(numbers); // Delete Range numbers in Console
-            workingroom.ShowList(numbers); // Show List in Console
+            //workingroom.FillList(numbers); // Random fill List of numbers
+            //workingroom.ShowList(numbers); // Show List in Console
+            //workingroom.DeleteRangeList(numbers); // Delete Range numbers in Console
+            //workingroom.ShowList(numbers); // Show List in Console
 
-            Console.ReadKey();
+            //Console.ReadKey();
             //////////////////////////////////////////////////////////////////////
+            // Task 2
+            WorkingRoom workingroom = new WorkingRoom(); // we add methods here           
+            var phonebook = new Dictionary<string, string>(); // we create dictionary here
 
+            while (true)
+            {                
+                workingroom.LoadPhoneBookData(ref phonebook); // we load PhoneBookData from file to memory here
+                workingroom.MainMenuTitle(); // main menu logo
+                Console.WriteLine("\nPlease enter the number for menu navigation: " +
+                "\n 1 - Show all PhoneBook Data." +
+                "\n 2 - Seach Number and User." +
+                "\n 3 - Add Number and User." +
+                "\n 4 - About Programm.");
+                int UserChoise;
+                int.TryParse(Console.ReadLine(), out UserChoise);
+
+                switch (UserChoise)
+                {
+                    case 1:Console.Clear();
+                        workingroom.PhoneBookData(phonebook);// show all PhoneBook
+                        break;
+                    case 2:
+                        workingroom.SearchNumberUser(phonebook);// search in PhoneBook by number
+                        break;
+                    case 3:
+                        workingroom.AddNumberUser(ref phonebook);// add new User with phone to PhoneBook
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Program Created by Ander(cooler-SAI)" +
+                                "\n v 1.1 (18.05.2022) Stavropol City, Russia" +
+                                "\n" +
+                                "\nPress any key to continue..."); // about program
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("\nWrong number! Please try again...");
+                        break;
+                }                
+                phonebook.Clear();
+                Console.Clear();
+            }
 
 
 
