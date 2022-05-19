dfa875b36ff1983913bcf84fb506092cac606df5
cooler-SAI <cooler19860212@yahoo.com>
2022-05-19 22:36:33 +0300

    Code: ,ore work here and there

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Study
 {
@@ -102,6 +104,11 @@ namespace Study
             //}
             #endregion
             //////////////////////////////////////////////////////////////////////
+
+            
+
+
+
 
 
 
