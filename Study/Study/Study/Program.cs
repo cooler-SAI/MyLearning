697b2f54c3982843060e554269375f1b5eeaa1f2
cooler-SAI <cooler19860212@yahoo.com>
2022-05-20 23:08:57 +0300

    Code: clearn up

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -5,16 +5,7 @@
         
         static void Main(string[] args)
         {
-           Dictionary<string, string> russian = new Dictionary<string, string>();
-
-            russian.Add("Hello", "Привет");
-            russian.Add("Bye", "ПОка");
-
-            foreach (var item in russian)
-            {
-                Console.WriteLine(item);
-            }
-           
+                     
 
 
 
