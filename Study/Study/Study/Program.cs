a8f02096c62b7e8169fd4bfe78c3bffbfa3aef7e
cooler-SAI <cooler19860212@yahoo.com>
2022-05-20 23:23:44 +0300

    Code: add test HashSet

--- Study/Study/Study/Program.cs
+++ Study/Study/Study/Program.cs
@@ -2,11 +2,23 @@
 {
     class Program
     {
-        
+        private static void HashPrint(ref HashSet<string> Print)
+        {
+            foreach (var item in Print)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
         static void Main(string[] args)
         {
-                     
+            var hash = new HashSet<string>();
+            hash.Add ("1");
+            hash.Add ("Server");
+            hash.Add ("2");
+            hash.Add ("coolOne!");
 
+            HashPrint(ref hash);
 
 
 
