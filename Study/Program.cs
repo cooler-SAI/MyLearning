7872760a1a4cc91dc86fa0f471d2a656a7ca2d2f
cooler-SAI <cooler19860212@yahoo.com>
2022-05-30 15:50:44 +0300

    Code: test up a little stuff))

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,7 +5,18 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            
+            int a = 0;
+            while (a <10000)
+            {
+                Console.Write("o_O! yuppi!!! ");
+                a++;
+                if (a == 8000)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\n\nProgram BROKEN!!! ");
+            Console.ReadLine();
             
            
         }
