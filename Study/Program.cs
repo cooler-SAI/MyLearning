015413d912f6e7ac27b1936bf96d2afa21cf17cc
cooler-SAI <cooler19860212@yahoo.com>
2022-05-30 15:43:16 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,19 +5,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            var a = 3 < 4;
-            var b = 5 == 6;
-            var c = b && a;
-
-            if (!b)
-            {
-                Console.WriteLine($"{b} is result ");
-
-            }
-            else
-            {
-                Console.WriteLine(c);
-            }
+            
             
            
         }
