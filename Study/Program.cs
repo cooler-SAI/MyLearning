b8581ebd5c868b90e2f7914d3fc9ce89703805ff
cooler-SAI <cooler19860212@yahoo.com>
2022-06-02 23:51:14 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -5,14 +5,7 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            var sum = 0;
-            while (true)
-            {
-                var line = Console.ReadLine();
-                if (line == "") break;
-                sum+=int.Parse(line);
-                Console.WriteLine(line);
-            }
+            
             
            
         }
