62b4be56503fda30a2be3fcdaa4fef8ac426c4e8
cooler-SAI <cooler19860212@yahoo.com>
2022-08-08 10:49:35 +0300

    Project: here, there - load all trash

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,16 +1,24 @@
-﻿
-namespace Study
+﻿namespace Study
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void HelloHere(int[] myArray, int i = 0)
         {
-            
-            
-           
+            if (i < myArray.Length)
+            {
+                Console.WriteLine(myArray[i]);
+                HelloHere(myArray, i + 1);
+            }
+            else
+            {
+                Console.WriteLine("The END here!!!");
+            }
         }
-        
-    }
-  
-}
 
+        private static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3 };
+            HelloHere(myArray);
+        }
+    }
+}
