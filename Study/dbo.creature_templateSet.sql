960664038fd6e3562a4bd8ab5703e7cab483e821
cooler-SAI <cooler19860212@yahoo.com>
2022-08-08 14:37:19 +0300

    Code: clearn up

--- Study/dbo.creature_templateSet.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[creature_templateSet] (
-    [Entry] INT            IDENTITY (1, 1) NOT NULL,
-    [Name]  VARCHAR(50) NULL,
-    [Level] INT NOT NULL,
-    CONSTRAINT [PK_creature_templateSet] PRIMARY KEY CLUSTERED ([Entry] ASC)
-);
-
