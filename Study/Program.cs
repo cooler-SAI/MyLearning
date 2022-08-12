fff8a755ff80cf7f170b660a9ee4131b0058027e
cooler-SAI <cooler19860212@yahoo.com>
2022-08-12 21:09:08 +0300

    Code: clearn up

--- Study/Program.cs
+++ Study/Program.cs
@@ -2,23 +2,8 @@
 {
     internal class Program
     {
-        private static void HelloHere(int[] myArray, int i = 0)
-        {
-            if (i < myArray.Length)
-            {
-                Console.WriteLine(myArray[i]);
-                HelloHere(myArray, i + 1);
-            }
-            else
-            {
-                Console.WriteLine("The END here!!!");
-            }
-        }
-
         private static void Main(string[] args)
         {
-            int[] myArray = { 1, 2, 3 };
-            HelloHere(myArray);
         }
     }
 }
